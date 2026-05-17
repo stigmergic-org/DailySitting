e24bbe5b95e4d5d50ab91b9edeba14777bc77796
Joel Thorstensson <joel@stigmergic.org>
2026-05-17 15:40:54 +0100

    chore(release): v0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "org.stigmergic.dailysitting"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.2.1"
     }
 
     signingConfigs {
